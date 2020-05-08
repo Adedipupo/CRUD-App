ca0ab42da0624f698ddf9b40a9387a009fcc3847
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-08 02:06:18 +0100

    m

--- src/forms/EditUserForm.js
+++ src/forms/EditUserForm.js
@@ -1,32 +1,49 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-const EditUserForm = props => {
-    const [user, setUser] = useState(props.currentUser)
+const EditUserForm = (props) => {
+  const [user, setUser] = useState(props.currentUser);
 
-    const handleInputChange = event => {
-        const { name, value } = event.target
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
 
-        setUser({ ...user, [name]: value })
-    }
+    setUser({ ...user, [name]: value });
+  };
 
-    return (
-        <form
-            onSubmit={event => {
-                event.preventDefault()
+  useEffect(() => {
+    setUser(props.currentUser);
+  }, [props]);
 
-                props.updateUser(user.id, user)
-            }}
-        >
-            <label>Name</label>
-            <input type="text" name="name" value={user.name} onChange={handleInputChange} />
-            <label>Username</label>
-            <input type="text" name="username" value={user.username} onChange={handleInputChange} />
-            <button>Update user</button>
-            <button onClick={() => props.setEditing(false)} className="button muted-button">
-                Cancel
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        props.updateUser(user.id, user);
+      }}
+    >
+      <label>Name</label>
+      <input
+        type="text"
+        name="name"
+        value={user.name}
+        onChange={handleInputChange}
+      />
+      <label>Username</label>
+      <input
+        type="text"
+        name="username"
+        value={user.username}
+        onChange={handleInputChange}
+      />
+      <button>Update user</button>
+      <button
+        onClick={() => props.setEditing(false)}
+        className="button muted-button"
+      >
+        Cancel
       </button>
-        </form>
-    )
-}
+    </form>
+  );
+};
 
 export default EditUserForm;
