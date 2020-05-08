55bcbba11200694be13f022941d708e7fd7bc359
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-08 01:12:50 +0100

    m

--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -9,6 +9,16 @@ const UserTable = () => (
                 <th>Actions</th>
             </tr>
         </thead>
+        <tbody>
+            <tr>
+                <td>Name data</td>
+                <td>Username data</td>
+                <td>
+                    <button className="button muted-button">Edit</button>
+                    <button className="button muted-button">Delete</button>
+                </td>
+            </tr>
+        </tbody>
     </table>
 );
 
