378e3483a36aca317f0337eb3a7e49b024077853
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-08 02:04:33 +0100

    m

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import UserTable from "./tables/UserTable";
 import AddUserForm from "./forms/addUserForm";
 
@@ -11,44 +11,57 @@ const App = () => {
 
   const [users, setUsers] = useState(usersData);
 
-  const addUser = user => {
-    user.id = users.length +1
+  const addUser = (user) => {
+    user.id = users.length + 1;
     setUsers([...users, user]);
-   }
+  };
 
-  const deleteUser = id => {
-    setUsers(users.filter(user => user.id !== id))
-  }
+  const deleteUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
 
-  const [editing, setEditing] = useState(false)
+  const [editing, setEditing] = useState(false);
 
-  const initialFormState = { id: null, name: '', username: '' }
+  const initialFormState = { id: null, name: "", username: "" };
 
-  const [currentUser, setCurrentUser] = useState(initialFormState)
+  const [currentUser, setCurrentUser] = useState(initialFormState);
 
-  const editRow = user => {
-    setEditing(true)
+  const editRow = (user) => {
+    setEditing(true);
 
-    setCurrentUser({ id: user.id, name: user.name, username: user.username })
-  }
+    setCurrentUser({ id: user.id, name: user.name, username: user.username });
+  };
 
   const updateUser = (id, updatedUser) => {
-    setEditing(false)
+    setEditing(false);
 
-    setUsers(users.map(user => (user.id === id ? updatedUser : user)))
-  }
-  
+    setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
+  };
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser}/>
+          {editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )}
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users} editRow={editRow} deleteUser={deleteUser}  />
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
         </div>
       </div>
     </div>
