26be8d4c73cd13b5160271f733533f8afef3fceb
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-08 01:26:10 +0100

    m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import UserTable from './tables/UserTable';
+import React from "react";
+import UserTable from "./tables/UserTable";
 
 const App = () => {
   const usersData = [
-    { id: 1, name: 'Tania', username: 'floppydiskette' },
-    { id: 2, name: 'Craig', username: 'siliconeidolon' },
-    { id: 3, name: 'Ben', username: 'benisphere' },
-  ]
+    { id: 1, name: "Tania", username: "floppydiskette" },
+    { id: 2, name: "Craig", username: "siliconeidolon" },
+    { id: 3, name: "Ben", username: "benisphere" },
+  ];
 
   const [users, setUsers] = useState(usersData);
 
@@ -19,11 +19,11 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable />
+          <UserTable users={users} />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
